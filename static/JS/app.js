1d451280db7f0fa8435a0754d3ec9d3c49474d84
rabbicraft <rabbicraft@outlook.com>
2024-07-18 08:28:05 -0700

    hi

---
+++ static/JS/app.js
@@ -0,0 +1,20 @@
+document.getElementById('continue-btn').addEventListener('click', function(event) {
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const passwordDiv = document.querySelector('.password-input');
+  const emailError = document.getElementById('email-error');
+  const passwordError = document.getElementById('password-error');
+  
+  if (!emailInput.value) {
+    emailError.classList.remove('hidden');
+  } else if (passwordDiv.style.display === 'none') {
+    // Hide email error message and show password input
+    emailError.classList.add('hidden');
+    passwordDiv.style.display = 'block';
+    passwordInput.focus();
+  } else if (!passwordInput.value) {
+    passwordError.classList.remove('hidden');
+  } else {
+    document.getElementById('form').submit();
+  }
+});
